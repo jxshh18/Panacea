afd10097e1204ce0f49336f56e407465ffaf2247
jxshh18 <joshobana462@gmail.com>
2025-11-14 15:40:33 +0800

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","panacea")
+$conn = mysqli_connect("localhost","root","1234","panacea")
         or die("Could not connect to the database.");
+
 
 ?>
